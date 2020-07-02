f83ad82fea167ffaea6c32b340378bd492228f66
vagrant <vagrant@localhost.localdomain>
2020-07-02 13:10:08 +0000

    fix shirine

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -15,7 +15,7 @@ require 'shrine/storage/s3'
 #     cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options),
 #     store: Shrine::Storage::S3.new(prefix: 'store', **s3_options)}
 # else
-#   require "shrine/storage/file_system"
+   require "shrine/storage/file_system"
 
    Shrine.storages = {
      cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
